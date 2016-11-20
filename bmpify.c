95b5309d9147d036784cbecfa8e4595b5448a50e
Miles Davis <davis.g.miles@gmail.com>
2016-11-20 14:54:26 +1030

    added structs and platform-specific defines/typedefs

--- bmpify.c
+++ bmpify.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
 #include "bmpify.h"
 
 int main(int argc, char *argv[])
 {
+	if(argc != 3)
+	{
+		printf("Usage: %s input-file output-file\n", argv[0]);
+		return 1;
+	}
+
 	return 0;
 }
--- bmpify.h
+++ bmpify.h
@@ -1,4 +1,44 @@
 #ifndef BMPIFY_H
 #define BMPIFY_H
 
+#ifdef _WIN32
+
+typedef int int32_t;
+typedef short int16_t;
+#define PACK(__Declaration__) __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
+
+#else
+
+#include <sys/types.h>
+#define PACK(__Declaration__) __Declaration__ __attribute__((__packed__))
+
+#endif
+
+PACK(struct fileHeader
+{
+	int16_t signature;
+	int32_t fileSize;
+	int16_t reservedA;
+	int16_t reservedB;
+	int32_t offset;
+});
+
+PACK(struct bitmapHeader
+{
+	int32_t headerSize;
+	int32_t width;
+	int32_t height;
+	int16_t planes;
+	int16_t bitCount;
+	int32_t compression;
+	int32_t imageSize;
+	int32_t xPPM;
+	int32_t yPPM;
+	int32_t colourPallet;
+	int32_t importantColours;
+});
+
+typedef struct fileHeader fileHeader_t;
+typedef struct bitmapHeader bitmapHeader_t;
+
 #endif
